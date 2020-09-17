3f16b360231e69e8a994a79153cb440791cd27d7
Marrione Nguyen <mtiamsic@gmail.com>
2020-09-17 16:03:10 -0700

    install inquirer

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
-// array of questions for user
-const questions = [
+// const inquirer = require("inquirer");
 
-];
+// user prompt for questioning
+// const promptUser = () => {
+//     return inquirer.prompt([
+//         {
+//             type: 'input',
+//             name: 'name',
+//             message: 'What is your project title?',
+//             validate: nameInput => {
+//                 if (nameInput) {
+//                     return true;
+//                 } else {
+//                     console.log('Please enter your project name!');
+//                     return false;
+//                 }
+//             }
+//         }
+//     ])
+// }
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+// promptUser()
+//     .catch(err => {git
+//         console.log(err);
+//     });
+
+// console.log(promptUser);
+
+
+// function to write README files
+// function writeToFile(fileName, data) {
+// }
 
 // function to initialize program
-function init() {
+// function init() {
 
-}
+// }
 
 // function call to initialize program
-init();
+// init();
