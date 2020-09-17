4bbc6cc115c078a7585b071bb207820b23961d59
Marrione Nguyen <mtiamsic@gmail.com>
2020-09-17 16:38:24 -0700

    questions tested

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-// const inquirer = require("inquirer");
+const inquirer = require("inquirer");
 
 // user prompt for questioning
-// const promptUser = () => {
-//     return inquirer.prompt([
-//         {
-//             type: 'input',
-//             name: 'name',
-//             message: 'What is your project title?',
-//             validate: nameInput => {
-//                 if (nameInput) {
-//                     return true;
-//                 } else {
-//                     console.log('Please enter your project name!');
-//                     return false;
-//                 }
-//             }
-//         }
-//     ])
-// }
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your project title?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project name!');
+                    return false;
+                }
+            }
+        }
+    ])
+};
 
-// promptUser()
-//     .catch(err => {git
-//         console.log(err);
-//     });
+promptUser()
+    .catch(err => {
+        console.log(err);
+    });
 
-// console.log(promptUser);
+
 
 
 // function to write README files
